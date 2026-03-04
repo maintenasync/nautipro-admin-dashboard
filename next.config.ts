a78e899fcb56cb5be3bc90f07b57d49e54f1f9d0
Rendra Tri <rendratrykusuma@gmail.com>
2026-03-04 22:49:32 +0700

    fix(security): path react2shell

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,45 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    eslint: {
-        // Warning: This allows production builds to successfully complete even if
-        // your project has ESLint errors.
-        ignoreDuringBuilds: true,
-    },
+    async headers() {
+        return [
+            {
+                // Menerapkan headers ini ke seluruh route
+                source: '/(.*)',
+                headers: [
+                    {
+                        key: 'X-DNS-Prefetch-Control',
+                        value: 'on'
+                    },
+                    {
+                        key: 'Strict-Transport-Security',
+                        value: 'max-age=63072000; includeSubDomains; preload'
+                    },
+                    {
+                        key: 'X-XSS-Protection',
+                        value: '1; mode=block'
+                    },
+                    {
+                        key: 'X-Frame-Options',
+                        value: 'SAMEORIGIN'
+                    },
+                    {
+                        key: 'X-Content-Type-Options',
+                        value: 'nosniff'
+                    },
+                    {
+                        key: 'Referrer-Policy',
+                        value: 'origin-when-cross-origin'
+                    },
+                    {
+                        key: 'Content-Security-Policy',
+                        // CSP Dasar: Izinkan gambar dari mana saja (seperti unpkg, avatar profil), batasi script/frame ke host asal.
+                        value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src * data: blob:; font-src 'self' data:; connect-src *;"
+                    }
+                ]
+            }
+        ];
+    }
 };
 
 export default nextConfig;
